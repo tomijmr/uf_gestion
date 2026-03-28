e4ddc1099954f8c667d3dea35278d76df8806daf
tomi <tms1csgo@gmail.com>
2026-03-28 11:31:37 -0300

    Update pedido_editar.php

--- public/pedido_editar.php
+++ public/pedido_editar.php
@@ -7,6 +7,7 @@ error_reporting(E_ALL);
 
 <?php
 require_once __DIR__ . '/../app/helpers.php';
+require_once __DIR__ . '/../app/db.php';
 if (isset($_GET['export_pedido'])) {
   $order_id = (int)($_GET['order_id'] ?? 0);
   $logo = url('favicon-96x96.png');
