c00e1a2a7031b117c51813c9d194240150a0cda8
tomi <tms1csgo@gmail.com>
2026-03-28 10:54:44 -0300

    Update pedidos.php

--- public/pedidos.php
+++ public/pedidos.php
@@ -1,4 +1,8 @@
 <?php
+// Mostrar errores para debug
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
 require_once __DIR__ . '/../app/auth.php';
 require_login();
 require_once __DIR__ . '/../app/db.php';
